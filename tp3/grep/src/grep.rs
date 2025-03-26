b727a675ed8efd68b64674dadc10c0aa84fe7920
FranCalveyra <franciscocalveyra24@gmail.com>
2025-03-25 22:48:29 -0300

    Made everything truly concurrent, added some copies of the bible in order to better test concurrency. Noticed performance improvements on conc and c-chunk approaches.

--- tp3/grep/src/grep.rs
+++ tp3/grep/src/grep.rs
@@ -1,60 +1,84 @@
-use std::thread;
-use crate::search_types::SearchType;
 use crate::file_processor::get_file_reader;
+use crate::search_types::SearchType;
 use crate::sequence_searcher::{find_sequence_in_file, find_sequence_in_file_per_chunk};
+use std::thread;
+use std::thread::JoinHandle;
 
-const CHUNK_SIZE: i32 = 500; // Line amount
+const CHUNK_SIZE: i32 = 20000; // Line amount
 
-
-trait Grep{
-    fn find_sequence_in_file(&self, pattern : &String, file_paths: Vec<String>) -> Vec<String>;
+trait Grep {
+    fn find_sequence_in_file(&self, pattern: &String, file_paths: Vec<String>) -> Vec<String>;
 }
 
-impl Grep for SearchType{
+impl Grep for SearchType {
     fn find_sequence_in_file(&self, pattern: &String, file_paths: Vec<String>) -> Vec<String> {
         let mut lines: Vec<String> = Vec::new();
         match self {
             SearchType::Sequential => {
                 lines = sequential_grep(&pattern, file_paths);
-            },
-            SearchType::Concurrent => { lines = concurrent_grep(&pattern, file_paths); },
-            SearchType::ChunkConcurrent => {lines = chunk_concurrent_grep(&pattern, file_paths);},
+            }
+            SearchType::Concurrent => {
+                lines = concurrent_grep(&pattern, file_paths);
+            }
+            SearchType::ChunkConcurrent => {
+                lines = chunk_concurrent_grep(&pattern, file_paths);
+            }
         }
         lines
     }
 }
 
-
-pub fn grep (search_type: SearchType, search_term: String, file_paths: Vec<String>) {
+pub fn grep(search_type: SearchType, search_term: String, file_paths: Vec<String>) {
     let lines = search_type.find_sequence_in_file(&search_term, file_paths);
 
     lines.iter().for_each(|line| println!("{}", line))
-
 }
 
 fn sequential_grep(search_term: &String, file_paths: Vec<String>) -> Vec<String> {
     file_paths
         .into_iter()
-            .map(|path| get_file_reader(path.to_string()).unwrap())
-            .map(|reader| find_sequence_in_file(reader, &search_term))
-        .flatten()
-        .collect()
-}
-
-fn concurrent_grep(search_term: &String, file_paths: Vec<String>) -> Vec<String> {
-    file_paths
-        .into_iter()
-        .map(|path| thread::spawn(move || get_file_reader(path.to_string()).unwrap()).join().unwrap())
+        .map(|path| get_file_reader(path.to_string()).unwrap())
         .map(|reader| find_sequence_in_file(reader, &search_term))
         .flatten()
         .collect()
 }
 
-fn chunk_concurrent_grep(search_term: &String, file_paths: Vec<String>)-> Vec<String> {
+fn concurrent_grep(search_term: &String, file_paths: Vec<String>) -> Vec<String> {
+    let mut handles: Vec<JoinHandle<Vec<String>>> = Vec::new();
     file_paths
         .into_iter()
-        .map(|path| thread::spawn(move || get_file_reader(path.to_string()).unwrap()).join().unwrap())
-        .map(|reader| find_sequence_in_file_per_chunk(reader, &search_term, CHUNK_SIZE))
+        .map(|path| {
+            let value = search_term.clone();
+            thread::spawn(move || {
+                find_sequence_in_file(get_file_reader(path.to_string()).unwrap(), &value)
+            })
+        })
+        .for_each(|handle| handles.push(handle));
+    handles
+        .into_iter()
+        .map(|handle| handle.join().unwrap())
+        .flatten()
+        .collect()
+}
+
+fn chunk_concurrent_grep(search_term: &String, file_paths: Vec<String>) -> Vec<String> {
+    let mut handles: Vec<JoinHandle<Vec<String>>> = Vec::new();
+    file_paths
+        .into_iter()
+        .map(|path| {
+            let value = search_term.clone();
+            thread::spawn(move || {
+                find_sequence_in_file_per_chunk(
+                    get_file_reader(path.to_string()).unwrap(),
+                    &value,
+                    CHUNK_SIZE,
+                )
+            })
+        })
+        .for_each(|handle| handles.push(handle));
+    handles
+        .into_iter()
+        .map(|handle| handle.join().unwrap())
         .flatten()
         .collect()
 }
--- tp3/grep/src/lib.rs
+++ tp3/grep/src/lib.rs
@@ -7,7 +7,6 @@ mod grep;
 #[cfg(test)]
 mod tests {
     use crate::file_processor::get_file_reader;
-    use crate::search_types::SearchType;
     use crate::sequence_searcher::find_sequence_in_file;
 
     #[test]
@@ -18,7 +17,6 @@ mod tests {
         let lines = find_sequence_in_file(
             file_reader.unwrap(),
             &"How".to_string(),
-            SearchType::Sequential,
         );
         assert_eq!("How dreary to be somebody!", &lines[0]);
         assert_eq!("How public, like a frog", &lines[1]);
