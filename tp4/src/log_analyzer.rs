29d73ef7f1fd19056318a4d2073506b3501ce0a4
FranCalveyra <franciscocalveyra24@gmail.com>
2025-04-15 15:33:44 -0300

    Changed imports

--- tp4/src/log_analyzer.rs
+++ tp4/src/log_analyzer.rs
@@ -1,6 +1,4 @@
 use grep_lib::sequence_searcher::find_sequence_in_file;
-use std::fs::File;
-use std::io::BufReader;
 
 pub fn analyze_logs(search_term: String, buf_reader: &Vec<String>) -> usize {
     find_sequence_in_file(buf_reader, &search_term).len()
